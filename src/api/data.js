409d57721fb7e05b79cc9c97942248f8df992c82
Wilker Paiva <wilkerpaiva1@yahoo.com>
2022-09-03 10:15:34 -0300

    Rotas atualizadas

--- src/api/data.js
+++ src/api/data.js
@@ -6,14 +6,14 @@ export default {
                 "id": "1",
                 "role": "FRONTEND DEVELOPER",
                 "title": "NETFLIX CLONE",
-                "img": "./src/assets/netflix-clone-home.png",
+                "img": "../assets/netflix-clone-home.png",
                 "description":"Neste projeto reconstruo a interface (UI/UX) da Netflix utilizando React e Tailwind CSS. O clone reproduz todas as funcionalidades da home original.",
             },
             {
                 "id": "2",
                 "role": "MOTION DESIGNER",
                 "title": "LIQUID TEXT",
-                "img": "./src/assets/liquid.png",
+                "img": "../assets/liquid.png",
                 "description":"Efeito de texto liquido utilizando recursos nativos do Adobe After Effects CC.",
             },
         ]
